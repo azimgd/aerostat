425838f265689aea10c57b740e0ead1a4761af09
Azim Gadzhiagayev <me@azimgd.com>
2015-09-24 07:02:12 +0200

    Cleaning index.js, exporting dependecies, adding example folder

---
+++ examples/repeating-delayed.js
@@ -0,0 +1,18 @@
+let manager = require('../index')({
+  infinite: true,
+  delay: 5000
+});
+
+manager.init('jobname', {
+  url: '/55febabac604810e14c098fa',
+  method: 'post',
+  payload: {
+    name: 'name',
+    surname: 'surname',
+    msg: 'message'
+  }
+}, (data) => {
+  console.log(data);
+}, (err) => {
+  console.log('failed request');
+});
