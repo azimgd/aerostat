64f324150847199319f1eae1f47492736e308b2e
Azim Gadzhiagayev <me@azimgd.com>
2015-10-12 16:08:32 +0200

    Messing up

--- src/request.js
+++ src/request.js
@@ -2,12 +2,10 @@ var axios = require('axios');
 
 class Request {
   constructor(options) {
-    console.log(options);
     this.root_url = options.root_url || false;
   }
 
   send(url, method, data) {
-    url = this.root_url + url;
     let params = {
       method: method,
       url: url,
