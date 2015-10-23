fd3b416de4688c61ecd4bca9688432ffc258d5a8
Azim Gadzhiagayev <me@azimgd.com>
2015-10-23 14:00:05 +0200

    ADD Request event hooks

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-module.exports = require('./src/manager');
+import producer from './lib/producer/producer';
+import consumer from './lib/consumer/consumer';
+import config from './lib/config';
+import init from './lib/init';
+init().start();
+
+const data = {
+  url: 'http://example.com/',
+  method: 'post',
+  payload: {
+    name: 'name',
+    surname: 'surname',
+    msg: 'message'
+  }
+};
+
+producer('heyyy', data).create();
+
+let jobConsumer = consumer('heyyy');
+jobConsumer.onValidate((res) => console.log(res, 'validate'))
+jobConsumer.onSuccess((res) => console.log(res, 'success'))
+jobConsumer.onFail((res) => console.log(res, 'fail'))
+jobConsumer.consume(jobConsumer.callback)
